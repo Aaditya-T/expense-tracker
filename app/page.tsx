e37e6ddb4e7a5c57da232a3d4744cd8ddf65a4f1
Ghatak18005 <aumghodasara15@gmail.com>
2024-04-19 10:28:19 +0530

    minor

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 				<div className="absolute left-0 right-0 top-3 z-20 flex items-center justify-between">
 					<Link href={'.url'} className="flex max-w-[180px] items-center p-3 text-2xl">
 						<Image src={logo} width={30} height={30} alt="KharchKhabri logo" className="mr-2" />
-						<span className="font-black tracking-[-0.03em] text-gray-900">Kharchkhabri</span>
+						<span className="font-black tracking-[-0.03em] text-gray-900">KharchKhabri</span>
 					</Link>
 					<div className="flex space-x-4">
 						<Link
@@ -55,51 +55,54 @@ export default function Home() {
 					</svg>
 				</div>
 
-	<div className="briefClass">
-    <div className="intro">
-      <h2>Effortless Expense Tracking: Take control of your finances in minutes a day</h2>
-      <p>Manage Your Finances Effortlessly
-        Gain control over your finances with our user-friendly expense tracker. Easily track your spending, set budgets, and achieve your financial goals. Stay organized, save time, and make informed decisions about your money.
+				<div className="briefClass">
+					<div className="intro">
+						<h2>Effortless Expense Tracking: Take control of your finances in minutes a day</h2>
+						<p>
+							Manage Your Finances Effortlessly Gain control over your finances with our user-friendly expense tracker.
+							Easily track your spending, set budgets, and achieve your financial goals. Stay organized, save time, and
+							make informed decisions about your money. Sign up now to start managing your money smarter!
+						</p>
+					</div>
+					{/* use tailwind to format image */}
+					<img src="/demo/overview.jpg" alt="Logo Image" className=" w-[350px] rounded-xl ml-5" />
+				</div>
 
-        Sign up now to start managing your money smarter!</p>
-    </div>
-		{/* use tailwind to format image */}
-      <img src="/demo/overview.jpg" alt="Logo Image" className=" w-[350px] rounded-xl ml-5" />
-  </div>
-
-  <b><h2 className="featureHeading">Features</h2></b><br />
-  <div className="containerClass">
-    <div className="feature">
-      <h2>User Authentication</h2>
-      <p>Allow users to register and securely log in to access their expense data.</p>
-    </div>
-    <div className="feature">
-      <h2>Expense Logging</h2>
-      <p>Provide an interface for users to manually enter their expenses, including date, amount, category, and description.</p>
-    </div>
-    <div className="feature">
-      <h2>Expense Categories</h2>
-      <p>Allow users to categorize expenses for better organization and insights into spending habits.</p>
-    </div>
-    <div className="feature">
-      <h2>Income Tracking</h2>
-      <p>Enable users to track their income sources to have a complete financial overview.</p>
-    </div>
-    <div className="feature">
-      <h2>Budgeting</h2>
-      <p>Allow users to set budgets for different expense categories and receive notifications when approaching or exceeding limits.</p>
-    </div>
-    <div className="feature">
-      <h2>Expense Visualization</h2>
-      <p>Present users with charts and graphs to visualize spending patterns and summaries.</p>
-    </div>
-  </div>
-
-
-
-				
+				<b>
+					<h2 className="featureHeading">Features</h2>
+				</b>
+				<br />
+				<div className="containerClass">
+					<div className="feature">
+						<h2>User Authentication</h2>
+						<p>Allow users to register and securely log in to access their expense data.</p>
+					</div>
+					<div className="feature">
+						<h2>Expense Logging</h2>
+						<p>
+							Provide an interface for users to manually enter their expenses, including date, amount, category, and
+							description.
+						</p>
+					</div>
+					<div className="feature">
+						<h2>Expense Categories</h2>
+						<p>Allow users to categorize expenses for better organization and insights into spending habits.</p>
+					</div>
+					<div className="feature">
+						<h2>Income Tracking</h2>
+						<p>Enable users to track their income sources to have a complete financial overview.</p>
+					</div>
+					<div className="feature">
+						<h2>Stock Market Budgeting</h2>
+						<p>Allow users to set budgets for different stock market activities.</p>
+					</div>
+					<div className="feature">
+						<h2>Expense Visualization</h2>
+						<p>Present users with charts and graphs to visualize spending patterns and summaries.</p>
+					</div>
+				</div>
 			</main>
-			<Footer/>
+			<Footer />
 		</div>
 	);
 }
